d02e1332c8f7e58103115ff1939c983c84830356
Tawaccul <teps2222@gmail.com>
2023-01-15 16:44:32 +0400

    color picker

---
+++ script.js
@@ -0,0 +1,60 @@
+const colorPickerBtn = document.querySelector('#color-picker');
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+const colorList = document.querySelector('.all-colors');
+const clearAll = document.querySelector('.clear-all')
+
+const copyColor = elem => {
+    navigator.clipboard.writeText(elem.dataset.color);
+    elem.innerText = "Copied";
+    setTimeout(() => elem.innerText = elem.dataset.color, 1000);
+}
+
+const showColors = () => {
+   
+    if(!pickedColors.length) return;
+    colorList.innerHTML = pickedColors.map(color => ` 
+    <li class="color">
+      <span class="rect" style="background: ${color}; border: 1px solid ${color == '#ffffff' ? '#ccc' : color}"></span>
+      <span class="value" data-color="${color}">${color}</span>
+    </li>`).join('');
+
+    document.querySelector('.picked-colors').classList.remove('hide');
+
+
+    document.querySelectorAll('.color').forEach(li => {
+        li.addEventListener('click', e => copyColor(e.currentTarget.lastElementChild));
+    })
+
+}
+
+showColors();
+
+const activateDropper =  () => {
+    document.body.style.display = 'none';
+    setTimeout(async () => {
+        try {
+        const eyeDropper = new EyeDropper();
+        const { sRGBHex } = await eyeDropper.open();
+        navigator.clipboard.writeText(sRGBHex);
+        
+        if(!pickedColors.includes(sRGBHex)){
+            pickedColors.push(sRGBHex);
+            localStorage.setItem('picked-colors', JSON.stringify(pickedColors));
+            showColors();
+        }
+        
+    } catch (e) {
+        console.log(e)
+    }
+    document.body.style.display = 'block';
+    }, 10)
+}
+
+const clearAllColors = () => {
+    pickedColors.length = 0;
+    localStorage.setItem('picked-colors', JSON.stringify(pickedColors));
+    document.querySelector('.picked-colors').classList.add('hide');
+}
+
+colorPickerBtn.addEventListener('click', activateDropper);
+clearAll.addEventListener('click', clearAllColors)
